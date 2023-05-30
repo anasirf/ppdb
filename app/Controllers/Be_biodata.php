32bba270e8dbf548c342808afd10f679da568c0e
anas.irfan13 <anas.irfan13@gmail.com>
2023-05-30 09:08:05 +0700

    update

--- app/Controllers/Be_biodata.php
+++ app/Controllers/Be_biodata.php
@@ -9,7 +9,7 @@ class Be_biodata extends BaseController
     public function index()
     {
         $session = session();
-        $id_siswa =   $session->get('id_siswa'); //ini tetap
+        $id_siswa = $session->get('id_siswa'); //ini tetap
 
         $Msiswa = new Msiswa(); // model disesuaikan dengan nama tabel
         $siswa = $Msiswa->find($id_siswa); //disesuaikan dengan nama tabel 
@@ -27,6 +27,10 @@ class Be_biodata extends BaseController
 
         $id = $this->request->getVar('id');
         $nama = $this->request->getVar('nama');
+        $nama_lengkap = $this->request->getVar('nama_lengkap');
+        $jk = $this->request->getVar('jk');
+        $nik = $this->request->getVar('nik');
+        $nokk = $this->request->getVar('nokk');
 
 
 
@@ -35,32 +39,34 @@ class Be_biodata extends BaseController
         //     'no_ppdb' => 'required|',
         //     'password' => 'required|min_length[6]',
         //     'repassword' => 'required|min_length[6]|matches[password]'
-        // ];
-
-        
+        // ];    
 
         // if ($this->validate($rules)) { //jika inputan sesuai dengan validasi
-            // controller code
+        // controller code
 
-            $Msiswa->transStart();
-            //insert table siswa
-            $data = [
-                'id' => $id,
-                'nama'    => $nama,
-            ];
-            $Msiswa->save($data); //esekusi database
+        $Msiswa->transStart();
+        //insert table siswa
+        $data = [
+            'id' => $id,
+            'nama' => $nama,
+            'nama_lengkap' => $nama_lengkap,
+            'jk' => $jk,
+            'nik' => $nik,
+            'nokk' => $nokk,
+        ];
+        $Msiswa->save($data); //esekusi database
 
 
 
-            $Msiswa->transComplete();
+        $Msiswa->transComplete();
 
 
-            if ($Msiswa->transStatus() === FALSE) {
-                return redirect()->to('/be_biodata');
-            }
-
+        if ($Msiswa->transStatus() === FALSE) {
             return redirect()->to('/be_biodata');
-            // return redirect()->to('/auth/login'); //ini jika minta langsung ke login
+        }
+
+        return redirect()->to('/be_biodata');
+        // return redirect()->to('/auth/login'); //ini jika minta langsung ke login
         // } else {
         //     // $data['validation'] = $this->validator;
         //     return redirect()->to('/be_biodata');
