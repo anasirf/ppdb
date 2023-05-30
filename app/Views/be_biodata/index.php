b140532a8d06ab1ceaf30eeafb8c5a13e21e03d1
anas.irfan13 <anas.irfan13@gmail.com>
2023-05-30 08:54:29 +0700

    biodata_done

--- app/Views/be_biodata/index.php
+++ app/Views/be_biodata/index.php
@@ -5,7 +5,7 @@
 </div>
 <div class="container">
     <form action="<?= site_url('/be_biodata/store') ?>" method="POST">
-    <input type="hidden" class="form-control" id="id" name="id" value="<?=@$siswa['id']?>">
+        <input type="hidden" class="form-control" id="id" name="id" value="<?= @$siswa['id'] ?>">
         <div class="mb-3">
             <label for="name" class="form-label">Nama Lengkap</label>
             <!-- 
@@ -14,12 +14,15 @@
                 jika id = "nama" maka name = "nama" 
                 karena name akan digunakan untuk variabel POST
             -->
-            <input type="text" class="form-control" id="nama" name="nama" value="<?=@$siswa['nama']?>" placeholder="Nama Peserta Didik">
-            <div id="nameHelp" class="form-text">Nama Peserta Didik sesuai dokumen resmi yang berlaku akte atau ijazah</div>
+            <input type="text" class="form-control" id="nama" name="nama" value="<?= @$siswa['nama'] ?>"
+                placeholder="Nama Peserta Didik">
+            <div id="nameHelp" class="form-text">Nama Peserta Didik sesuai dokumen resmi yang berlaku akte atau ijazah
+            </div>
         </div>
         <div class="mb-3">
             <label for="nisn" class="form-label">NISN</label>
-            <input type="text" class="form-control" id="nisn" name="nisn"  value="<?=@$siswa['nisn']?>" placeholder="Nomor Induk Siswa Nasional">
+            <input type="text" class="form-control" id="nisn" name="nisn" value="<?= @$siswa['nisn'] ?>"
+                placeholder="Nomor Induk Siswa Nasional">
         </div>
         <div class="mb-3">
             <label for="jk" class="form-label">Jenis Kelamin</label>
@@ -30,24 +33,30 @@
                 <label class="form-check-label" for="inlineRadio1">Laki-laki</label>
             </div>
             <div class="form-check form-check-inline">
-                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="perempuan" value="jk_perempuan">
+                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="perempuan"
+                    value="jk_perempuan">
                 <label class="form-check-label" for="inlineRadio2">Perempuan</label>
             </div>
         </div>
 
         <div class="mb-3">
             <label for="nik" class="form-label">NIK/No. KITAS (Untuk WNA)</label>
-            <input type="text" class="form-control" id="nik" name="nik"  value="<?=@$siswa['nik']?>" aria-describedby="nikHelp" placeholder="Nomor Induk Kependudukan">
-            <div id="nikHelp" class="form-text">Nomor Induk Kependudukan yang tercantum pada KK / KTP /KIA bagi WNA diisi menggunakan nomor Kartu Izin Tinggal Terbatas (KITAS).</div>
+            <input type="text" class="form-control" id="nik" name="nik" value="<?= @$siswa['nik'] ?>"
+                aria-describedby="nikHelp" placeholder="Nomor Induk Kependudukan">
+            <div id="nikHelp" class="form-text">Nomor Induk Kependudukan yang tercantum pada KK / KTP /KIA bagi WNA
+                diisi menggunakan nomor Kartu Izin Tinggal Terbatas (KITAS).</div>
         </div>
         <div class="mb-3">
             <label for="nokk" class="form-label">No. KK</label>
-            <input type="text" class="form-control" id="nokk" name="nokk" value="<?=@$siswa['nokk']?>" placeholder="98921389138">
+            <input type="text" class="form-control" id="nokk" name="nokk" value="<?= @$siswa['nokk'] ?>"
+                placeholder="98921389138">
         </div>
         <div class=" mb-3">
             <label for="tempat_lahir" class="form-label">Tempat Lahir</label>
-            <input type="text" class="form-control" id="tempat_lahir" name="tempat_lahir" value="<?=@$siswa['tempat_lahir']?>" placeholder="Yogyakarta">
-            <div id="tempat_lahirHelp" class="form-text">Tempat Lahir Peserta Didik sesuai dokumen resmi yang berlaku akte atau ijazah</div>
+            <input type="text" class="form-control" id="tempat_lahir" name="tempat_lahir"
+                value="<?= @$siswa['tempat_lahir'] ?>" placeholder="Yogyakarta">
+            <div id="tempat_lahirHelp" class="form-text">Tempat Lahir Peserta Didik sesuai dokumen resmi yang berlaku
+                akte atau ijazah</div>
         </div>
         <div class="mb-3">
             <label for="tanggal_lahir">Tanggal Lahir:</label>
@@ -55,8 +64,10 @@
         </div>
         <div class="mb-3">
             <label for="no_akta" class="form-label">No Registerasi Akta</label>
-            <input type="text" class="form-control" id="no_akta" name="no_akta" placeholder="2075/K/2004">
-            <div id="no_aktaHelp" class="form-text">Nomor Registerasi Akta kelahiran. Nomor yang dimaksud umumnya terletak pada bagian tengah atas lembar kutipan akta kelahiran</div>
+            <input type="text" class="form-control" id="no_akta" name="no_akta" value="<?= @$siswa['no_akta'] ?>"
+                placeholder="2075/K/2004">
+            <div id="no_aktaHelp" class="form-text">Nomor Registerasi Akta kelahiran. Nomor yang dimaksud umumnya
+                terletak pada bagian tengah atas lembar kutipan akta kelahiran</div>
         </div>
         <div class="mb-3">
             <!-- name untuk agama? -->
@@ -75,39 +86,45 @@
         <!-- jika dijadikan radio button ya dan tidak bisakah? -->
         <div class="mb-3">
             <label for="kebutuhan_khusus" class="form-label">Berkebutuhan Khusus</label>
-            <input type="text" class="form-control" id="k_khusus" name="k_khusus">
-            <div id="kebutuhan_khususHelp" class="form-text">Jika tidak memiliki kebutuhan khusus bisa diisi tidak atau -</div>
+            <input type="text" class="form-control" id="k_khusus" name="k_khusus" value="<?= @$siswa['k_khusus'] ?>">
+            <div id="kebutuhan_khususHelp" class="form-text">Jika tidak memiliki kebutuhan khusus bisa diisi "tidak"
+                atau "-" </div>
         </div>
         <div class="mb-3">
             <label for="alamat" class="form-label">Alamat Tempat Tinggal</label>
-            <input type="text" class="form-control" id="alamat_tinggal" name="alamat_tinggal" placeholder="Jl. Semangka No. XXIV">
+            <input type="text" class="form-control" id="alamat_tinggal" name="alamat_tinggal"
+                value="<?= @$siswa['alamat_tinggal'] ?>" placeholder="Jl. Semangka No. XXIV">
             <div id="alamatHelp" class="form-text">Alamat tinggal peserta didik diisi secara lengkap.</div>
         </div>
         <div class="row">
             <div class="col-3">
                 <label for="rt" class="form-label">RT </label>
-                <input type="text" class="form-control" id="rt" name="rt" placeholder="05">
+                <input type="text" class="form-control" id="rt" name="rt" value="<?= @$siswa['rt'] ?>" placeholder="05">
             </div>
             <div class="col-3">
                 <label for="rw" class="form-label">RW </label>
-                <input type="text" class="form-control" id="rw" name="rw" placeholder="10">
+                <input type="text" class="form-control" id="rw" name="rw" value="<?= @$siswa['rw'] ?>" placeholder="10">
             </div>
         </div>
         <div class="mb-3">
             <label for="dusun" class="form-label">Dusun </label>
-            <input type="text" class="form-control" id="dusun" name="dusun" placeholder="Dusun Kuningan">
+            <input type="text" class="form-control" id="dusun" name="dusun" value="<?= @$siswa['dusun'] ?>"
+                placeholder="Dusun Kuningan">
         </div>
         <div class="mb-3">
             <label for="kelurahan" class="form-label">Kelurahan </label>
-            <input type="text" class="form-control" id="kelurahan" name="kelurahan" placeholder="Kelurahan Sidobali">
+            <input type="text" class="form-control" id="kelurahan" name="kelurahan" value="<?= @$siswa['kelurahan'] ?>"
+                placeholder="Kelurahan Sidobali">
         </div>
         <div class="mb-3">
             <label for="kecamatan" class="form-label">Kecamatan </label>
-            <input type="text" class="form-control" id="kecamatan" name="kecamatan" placeholder="Kecamatan Umbulharjo">
+            <input type="text" class="form-control" id="kecamatan" name="kecamatan" value="<?= @$siswa['kecamatan'] ?>"
+                placeholder="Kecamatan Umbulharjo">
         </div>
         <div class="mb-3">
             <label for="kd_pos" class="form-label">Kode Pos </label>
-            <input type="text" class="form-control" id="kd_pos" name="kd_pos" placeholder="516517">
+            <input type="text" class="form-control" id="kd_pos" name="kd_pos" value="<?= @$siswa['kd_pos'] ?>"
+                placeholder="516517">
         </div>
         <!--tempat tinggal sama dengan agama menggunakan varchar option value disamakan dengan option name -->
         <div class="mb-3">
@@ -125,12 +142,15 @@
         </div>
         <div class="mb-3">
             <label for="m_transportasi" class="form-label">Moda Transportasi </label>
-            <input type="text" class="form-control" id="m_transportasi" name="m_transportasi" placeholder="Sepeda Motor/Bus/Mobil Pribadi">
-            <div id="m_transportasiHelp" class="form-text">Jenis transportasi utama atau yang paling sering digunakan peserta didik untuk berangkat ke sekolah contoh sepeda motor.</div>
+            <input type="text" class="form-control" id="m_transportasi" name="m_transportasi"
+                value="<?= @$siswa['m_transportasi'] ?>" placeholder="Sepeda Motor/Bus/Mobil Pribadi">
+            <div id="m_transportasiHelp" class="form-text">Jenis transportasi utama atau yang paling sering digunakan
+                peserta didik untuk berangkat ke sekolah contoh sepeda motor.</div>
         </div>
         <div class="mb-3">
             <label for="anak_ke" class="form-label">Anak keberapa</label>
-            <input type="text" class="form-control" id="anak_ke" name="anak_ke" placeholder="1 atau 2 atau 3">
+            <input type="text" class="form-control" id="anak_ke" name="anak_ke" value="<?= @$siswa['anak_ke'] ?>"
+                placeholder="1 atau 2 atau 3">
             <div id="anak_keHelp" class="form-text">Anak keberapa mengacu pada dokumen di KK.</div>
         </div>
         <!--radio button lagi disamakan dengan kebutuhan khusus -->
@@ -142,55 +162,43 @@
                 <label class="form-check-label" for="inlineRadio1">Ya</label>
             </div>
             <div class="form-check form-check-inline">
-                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="tidak_ada_kip" value="tidak_ada_kip">
+                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="tidak_ada_kip"
+                    value="tidak_ada_kip">
                 <label class="form-check-label" for="inlineRadio2">Tidak</label>
             </div>
             <div class="mb-3">
                 <label for="anak_ke" class="form-label">No KIP</label>
-                <input type="text" class="form-control" id="nokip" name="nokip" placeholder="37XOPDF">
+                <input type="text" class="form-control" id="nokip" name="nokip" value="<?= @$siswa['nokip'] ?>"
+                    placeholder="37XOPDF">
                 <div id="anak_keHelp" class="form-text">Nomor yang tertera pada Kartu Indonesia Pintar.</div>
             </div>
         </div>
-        <!--
-        <div class="mb-3">
-            <label for="tb" class="form-label">Tinggi Badan</label>
-            <input type="text" class="form-control" id="tb">
-            <div id="tbHelp" class="form-text">Tinggi Badan Peserta didik</div>
-        </div>
-        <div class="mb-3">
-            <label for="bb" class="form-label">Berat Badan</label>
-            <input type="text" class="form-control" id="bb">
-            <div id="bbHelp" class="form-text">Berat Badan Peserta didik</div>
-        </div>
-        <div class="mb-3">
-            <label for="lk" class="form-label">Lingkar Kepala</label>
-            <input type="text" class="form-control" id="lk">
-            <div id="lkHelp" class="form-text">Lingkar Kepala Peserta didik</div>
-        </div> -->
         <div class="row">
             <div class="col-3">
                 <label for="tb" class="form-label">Tinggi Badan </label>
-                <input type="text" class="form-control" id="tb" name="tb" placeholder="145">
+                <input type="text" class="form-control" id="tb" name="tb" value="<?= @$siswa['tb'] ?>"
+                    placeholder="145">
             </div>
             <div class="col-3">
                 <label for="bb" class="form-label">Berat Badan </label>
-                <input type="text" class="form-control" id="rw" name="bb" placeholder="65">
+                <input type="text" class="form-control" id="rw" name="bb" value="<?= @$siswa['bb'] ?>" placeholder="65">
             </div>
             <div class="col-3">
                 <label for="lk" class="form-label">Lingkar Kepala </label>
-                <input type="text" class="form-control" id="lk" name="lk" placeholder="35">
+                <input type="text" class="form-control" id="lk" name="lk" value="<?= @$siswa['lk'] ?>" placeholder="35">
             </div>
         </div>
         <!-- jarak rumah dijadikan radio button bisakah? jika kurang dari 1km dan lebih dari 1km -->
         <div class="mb-3">
             <label for="jarak" class="form-label">Jarak Rumah</label>
-            <input type="text" class="form-control" id="jarak">
+            <input type="text" class="form-control" id="jarak" value="<?= @$siswa['jarak'] ?>">
             <div id="jarakHelp" class="form-text">Jarak Tempat Tinggal Peserta didik ke sekolah</div>
         </div>
         <!-- hobi atau kegemaran jika dibuat 3 kolom kebawah bagaimana databasenya? -->
         <div class="mb-3">
             <label for="hobi" class="form-label">Hobi atau Kegemaran</label>
-            <select class="form-select js-example-basic-multiple" aria-label="Default select example" multiple="multiple">
+            <select class="form-select js-example-basic-multiple" aria-label="Default select example"
+                multiple="multiple">
                 <option value="1">Islam</option>
                 <option value="2">Kristen Protestan</option>
                 <option value="3">Katholik</option>
@@ -203,7 +211,8 @@
         </div>
         <div class="mb-3">
             <label for="bahasa" class="form-label">Bahasa</label>
-            <input type="text" class="form-control" id="bahasa" name="bahasa" placeholder="Jawa/Sunda">
+            <input type="text" class="form-control" id="bahasa" name="bahasa" value="<?= @$siswa['bahasa'] ?>"
+                placeholder="Jawa/Sunda">
             <div id="bahasaHelp" class="form-text">Bahasa yang digunakan sehari hari oleh peserta didik</div>
         </div>
 
@@ -219,7 +228,7 @@
 <?= $this->endSection() ?>
 <?= $this->section('js') ?>
 <script>
-    $(document).ready(function() {
+    $(document).ready(function () {
         $('.js-example-basic-multiple').select2();
     });
 </script>
